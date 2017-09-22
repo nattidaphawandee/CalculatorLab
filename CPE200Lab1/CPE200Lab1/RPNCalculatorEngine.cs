a0dcb768a52cd00d2b977aa18e2206214b6a9084
nattidaphawandee <31128775+nattidaphawandee@users.noreply.github.com>
2017-09-22 16:24:57 +0700

    เสร็จ

--- CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
+++ CPE200Lab1/CPE200Lab1/RPNCalculatorEngine.cs
@@ -10,22 +10,49 @@ namespace CPE200Lab1
     {
         public new string Process(string str)
         {
+            if (str == "" || str == null) return "E";
             Stack<string> rpnStack = new Stack<string>();
             List<string> parts = str.Split(' ').ToList<string>();
             string result;
             string firstOperand, secondOperand;
+            int number = 0, operate = 0;
+
 
             foreach (string token in parts)
             {
+                if (token.Length > 1)
+                {
+                    char num, sign;
+                    sign = Convert.ToChar(token.Substring(0, 1));
+                    num = Convert.ToChar(token.Substring(1, 1));
+                    if (sign == '+' || sign == 'X' || sign == '÷')
+                    {
+                        return "E";
+                    }
+                    if (num == '-' || num == 'X' || num == '+' || num == '÷')
+                    {
+                        return "E";
+                    }
+                }
+
+
+
+
                 if (isNumber(token))
                 {
+                    number++;
                     rpnStack.Push(token);
                 }
                 else if (isOperator(token))
                 {
                     //FIXME, what if there is only one left in stack?
-                    secondOperand = rpnStack.Pop();
-                    firstOperand = rpnStack.Pop();
+                    operate++;
+                    try
+                    {
+                        secondOperand = rpnStack.Pop();
+                        firstOperand = rpnStack.Pop();
+                    }
+                    catch { return "E"; }
                     result = calculate(token, firstOperand, secondOperand, 4);
                     if (result is "E")
                     {
@@ -35,7 +62,11 @@ namespace CPE200Lab1
                 }
             }
             //FIXME, what if there is more than one, or zero, items in the stack?
-            result = rpnStack.Pop();
+
+            if (number - 1 != operate) return "E";
+
+            try { result = rpnStack.Pop(); }
+            catch { return "E"; }
             return result;
         }
     }
